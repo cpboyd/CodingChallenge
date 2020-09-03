b1744bb84c17fbb78b78d564a717caada3ea5b2f
Christopher Boyd <xpboyd@gmail.com>
2020-09-03 17:23:29 -0400

    fix(FamilyTreeGenerator): ensure test for 33 passes

--- CodingChallenge.FamilyTree.Tests/FamilyTreeGenerator.cs
+++ CodingChallenge.FamilyTree.Tests/FamilyTreeGenerator.cs
@@ -13,6 +13,8 @@ namespace CodingChallenge.FamilyTree.Tests
             var hierarchySpec = Builder<HierarchySpec<Person>>.CreateNew()
                 .With(h => h.AddMethod, (p1, p2) => p1.Descendants.Add(p2))
                 .With(h => h.Depth = 7)
+                // Specify MinimumChildren to ensure Name33 gets added
+                .With(h => h.MinimumChildren = 2)
                 .With(h => h.MaximumChildren = 3)
                 .With(h => h.NumberOfRoots = 1).Build();
 
